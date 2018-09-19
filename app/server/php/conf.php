d68a89f245bdbb56d1bd5789d882ffe0ca2354f4
Edgar Guzman <anonimyusdj@gmail.com>
2018-09-19 16:48:55 -0600

    Update conf.php
    Configuracion de base de datos

--- app/server/php/conf.php
+++ app/server/php/conf.php
@@ -1,8 +1,8 @@
 <?php
-  define('HOST', 'localhost');
-  define('DBNAME','siteOctavio');
-  define('DBUSER','anonimyusdj');
-  define('DBPASS','anonimyusdj');
+  define('HOST', 'us-cdbr-iron-east-01.cleardb.net');
+  define('DBNAME','heroku_948e82f2e820e09');
+  define('DBUSER','bc28e80f74a2cb');
+  define('DBPASS','10db363e');
   define('DBPORT', '3306');
   define('DBCHARSET', 'utf8');
 
